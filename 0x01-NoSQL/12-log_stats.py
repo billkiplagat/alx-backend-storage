2b263e4d18535ace22b36810636cf0d0b138b620
billkiplagat <bill.kiplagat.7@gmail.com>
2024-03-26 08:18:15 -0400

    Mandatory completed

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''Task 12's module.
+'''
+from pymongo import MongoClient
+
+
+def print_nginx_request_logs():
+    '''Prints stats about Nginx request logs.
+    '''
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        req_count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, req_count))
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(status_checks_count))
+
+
+if __name__ == '__main__':
+    print_nginx_request_logs()
+
